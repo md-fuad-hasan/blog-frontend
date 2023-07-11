92711c0dc01778298399962716390a94f9baf8b9
Fuad Hasan <fuad.ice.ru@gmail.com>
2023-07-11 16:04:17 +0600

    Preparing the frontend of a blog website

---
+++ src/Components/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+class Main extends Component{
+
+    render(){
+
+        return(
+            <div>
+                <div className="container">
+
+                    <h2>This is a blog website</h2>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;    //  import in ../App.js
